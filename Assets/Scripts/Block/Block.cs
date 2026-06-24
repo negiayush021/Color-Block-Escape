bd3fe0fd8d8efa10ec6eebbb22acc4bcccbdbba1
Ayush <negiayush1055@gmail.com>
2026-06-24 15:57:24 +0530

    feat - create shake -effect on wrong move

--- Assets/Scripts/Block/Block.cs
+++ Assets/Scripts/Block/Block.cs
@@ -1,5 +1,7 @@
+using Solo.MOST_IN_ONE;
 using System.Collections;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class Block : MonoBehaviour
 {
@@ -7,6 +9,7 @@ public class Block : MonoBehaviour
     public Vector2Int GridPosition;
 
     private bool IsMoving = false;
+    
 
     private void Start()
     {
@@ -24,12 +27,16 @@ public class Block : MonoBehaviour
 
         if (targetCell == null)
         {
+            StartCoroutine(shakeEffect(direction));
+            LightImpactHaptic();
             Debug.Log("Out of bounds");
             return;
         }
 
         if (!GridManager.instance.CanMoveToCell(targetCell.X, targetCell.Y))
         {
+            StartCoroutine(shakeEffect(direction));
+            LightImpactHaptic();
             Debug.Log("Cannot move");
             return;
         }
@@ -71,4 +78,34 @@ public class Block : MonoBehaviour
 
     }
 
+    IEnumerator shakeEffect(Vector2Int direction)
+    {
+        float shakeDuration = 0.2f;
+        float elapsed = 0f;
+        float shakeMagnitude = 0.08f;
+        float shakeSpeed = 40f;
+        Vector3 originalPos = transform.position;
+
+        while (elapsed < shakeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float offset = Mathf.Sin(elapsed * shakeSpeed) * shakeMagnitude;
+            if(direction == Vector2Int.right || direction == Vector2Int.left)
+            {
+                transform.position = originalPos + new Vector3(0f, 0f, offset);
+            }
+            else
+            {
+                transform.position = originalPos + new Vector3(offset, 0f, 0f);
+            }
+            
+            yield return null;
+        }
+        transform.position = originalPos;
+    }
+
+    public void LightImpactHaptic()
+    {
+        MOST_HapticFeedback.Generate(MOST_HapticFeedback.HapticTypes.LightImpact);
+    }
 }
